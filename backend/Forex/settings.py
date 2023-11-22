f3ffbcd16d5ff430bb9b0122b8504830eead16fb
N3xTr1Me <destriker17@gmail.com>
2023-11-22 19:24:42 +0300

    Simple CRUD app with MongoDB integration created

--- backend/Forex/settings.py
+++ backend/Forex/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "viewer"
 ]
 
 MIDDLEWARE = [
@@ -70,15 +71,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Forex.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
