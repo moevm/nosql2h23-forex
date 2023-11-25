61e6fb47bd394995a5ec77bfcc2f354bad191121
Prokhorov Boris <deimos.75@mail.ru>
2023-11-25 20:13:38 +0300

    rm FE project

--- prototype/frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router'
-import { MainComponent } from './pages/main/main.component'
-import { ArchiveComponent } from './pages/archive/archive.component'
-
-export const routes: Routes = [
-  { path: '', component: MainComponent },
-  { path: 'archive', component: ArchiveComponent },
-]
